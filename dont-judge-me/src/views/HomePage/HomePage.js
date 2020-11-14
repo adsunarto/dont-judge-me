5ff75a621e6d1e614decf4d9a46c4c9f0f5bb70f
Steven Lu <sylfw6@mst.edu>
2020-11-14 17:09:11 -0600

    Updated home page

--- dont-judge-me/src/views/HomePage/HomePage.js
+++ dont-judge-me/src/views/HomePage/HomePage.js
@@ -13,12 +13,19 @@ const HomePage = () => {
     return (
         <Fragment>
             <div className="main">
-                <h1>Don't judge me for what I like to read</h1>
-                <input type="text" onChange={(e) => set_url(e.target.value)}></input>
-                {url}
-                <button type="button" onClick={timesify}>
-                    Click Here
-                </button>
+                <div className="header-container">
+                    <h1>Don't judge me for what I like to read</h1>
+                    <div className="link-field">
+                        <div>
+                            <div className="input">
+                                <input type="text" placeholder="Enter Link" onChange={(e) => set_url(e.target.value)}></input>
+                                <button type="button" onClick={timesify}>
+                                Hide my shame
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Fragment>
     );
